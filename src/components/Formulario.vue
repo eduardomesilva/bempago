94e4557f77c9f5fde7b7060a0dc6d0b7be8c16ff
Eduardo Messias <messiasgit@outlook.com>
2022-07-13 18:51:47 -0300

    final

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,90 +1,84 @@
 <template>
    <div class="common-layout">
     <el-container>
-      <el-header>
-        <h1> Compre adesivos rápido e facíl</h1>
-      </el-header>
-    <el-container>
-        <el-aside width="600px">Aside
-            <el-main>
+       <el-main>
+        <el-row class="row-bg">
+            <h3>Compre Adesivos Rápido e Facíl</h3>
+            <el-col>
+                <h3>Valor Unitario dos Adesivos R$ 5.00</h3>
+            </el-col>
+        </el-row>
+            <el-col>
+                <h3> Escolha qual adesivo comprar</h3>
+            </el-col>
+
+            <div>
+                <el-col> <el-checkbox-button v-model="vue" label="Veu.js" size="large" /> </el-col>
+                <el-col> <el-checkbox-button v-model="react" label="React" size="large" /> </el-col>
+                <el-col> <el-checkbox-button v-model="angular" label="Angular" size="large" /> </el-col>
+            </div>
+            <el-row class="row-bg" justify="center">
+                <h3> Informe a quantidade de adesivos </h3>
+            </el-row>
+            <el-row>
                 <el-col>
-                    <span> Escolha qual adesivo comprar</span>
-                    {{quantidade}}
+                    <el-input-number v-model="num" @change="handleChange"  size="large" />
                 </el-col>
-                <div>
-                    <el-col> <el-checkbox v-model="vue" label="Veu.js" size="large" /> </el-col>
-                    <el-col> <el-checkbox v-model="react" label="React" size="large" /> </el-col>
-                    <el-col> <el-checkbox v-model="angular" label="Angular" size="large" /> </el-col>
-                </div>
-                <el-row class="row-bg" justify="center">
-                    <span> Informe a quantidade de adesivos </span>
-                </el-row>
-                <el-row>
-                    <el-col>
-                        <el-input-number v-model="num" @change="handleChange" />
-                    </el-col>
-                </el-row>
-                <el-row class="row-bg" justify="center">
-                    <el-col :span="12">
-                        <el-input 
-                            v-model="textArea"
-                            :rows="4"
-                            type="textarea"
-                            background="grey"
-                            placeholder="Alguma duvida?"
-                        />
-                    </el-col>
-                </el-row>
-                 <el-button type="success" plain>Enviar</el-button>
-                <el-row>
-                </el-row>
-            </el-main>
-        </el-aside>
-        <el-container>
-            <el-main>
-                <el-row class="row-bg" justify="center">
-                    <a>Formas de Pagamentos</a>
-                </el-row>
-                <div>
-                    <el-checkbox v-model="cartao" label="Cartão" size="large" />
-                    <el-checkbox v-model="boleto" label="Boleto" size="large" /> 
-                    <el-checkbox v-model="pix" label="Pix" size="large" /> 
-                </div>
+            </el-row>
+            <el-row class="row-bg" justify="center">
+                <el-col :span="6">
+                    <el-input 
+                        v-model="textArea"
+                        :rows="4"
+                        type="textarea"
+                        background="grey"
+                        placeholder="Alguma duvida?"
+                    />
+                </el-col>
+            </el-row>
+            <el-button type="success">Enviar</el-button>
+            <el-row class="row-bg" justify="center">
                 <el-col>
-                    <el-button @click="formaPagamentoCartao" type="success" plain>Comprar</el-button>
+                    <h3>Formas de Pagamentos</h3>
                 </el-col>
-                <el-row class="row-bg" justify="center" v-show="pagarCartao">
-                    <el-col :span="8">
-                        <div class="pag">
-                            <div class="client">
-                                <el-input v-model="orde.nome" placeholder="Nome" />
-                                <el-input v-model="orde.sobrenome" placeholder="Sobrenome" />
-                                <el-input v-model="orde.cpf" placeholder="CPF" />
-                                <el-input v-model="orde.email" placeholder="email" />
-                                <el-input v-model="orde.numero_cartao" placeholder="Numero do cartão" />
-                                <el-col :span="4">
-                                    <el-input v-model="orde.data_validade" placeholder="validade"/>
-                                    <el-input v-model="orde.cvv" maxlength="3" placeholder="CVV" />
-                                </el-col>
-                            </div>
+                <el-col>
+                    <el-checkbox-button v-model="cartao" label="Cartão" size="large" />
+                    <el-checkbox-button v-model="boleto" label="Boleto" size="large" /> 
+                    <el-checkbox-button v-model="pix" label="Pix" size="large" /> 
+                </el-col>
+            </el-row>
+            <el-col>
+                <el-button :span="6" @click="formaPagamentoCartao" type="success">Finalizar Compra</el-button>
+            </el-col>
+            <el-row class="row-bg" justify="center" v-show="pagarCartao">
+                <el-col :span="8">
+                    <div class="pag">
+                        <div class="client">
+                            <el-input v-model="orde.nome" placeholder="Nome" />
+                            <el-input v-model="orde.sobrenome" placeholder="Sobrenome" />
+                            <el-input v-model="orde.cpf" placeholder="CPF" />
+                            <el-input v-model="orde.email" placeholder="email" />
+                            <el-input v-model="orde.numero_cartao" placeholder="Numero do cartão" />
+                            <el-col :span="4">
+                                <el-input v-model="orde.data_validade" placeholder="validade"/>
+                                <el-input v-model="orde.cvv" maxlength="3" placeholder="CVV" />
+                            </el-col>
                         </div>
-                    </el-col>
-                    <el-col>
-                        <el-button type="success" plain>Pagar</el-button>
-                    </el-col>
+                    </div>
+                </el-col>
+                <el-col>
+                    <el-button type="success">Pagar</el-button>
+                </el-col>
                 </el-row>
                 <el-row class="row-bg" justify="center" v-show="pagarPix">
                 <h1>Aqui vai o qrcode</h1>
                 </el-row>
                 <el-row class="row-bg" justify="center" v-show="pagarBoleto">
-                <h1>Aqui vai o Boleto</h1>
+                <h1>Olá senhor {{orde.nome}}, o boleto foi enviado para seu email, obrigado pela compra</h1>
                 </el-row>
             </el-main>
-            <el-footer>Footer</el-footer>
         </el-container>
-    </el-container>
-    </el-container>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -123,6 +117,7 @@ export default {
         compraAdesivos: function(){
             if(this.vue === true || this.react === true || this.angular == true){
                 this.quantidade = this.num * this.valorAdesivos
+                let valor
                 }
              
         },
@@ -160,7 +155,8 @@ export default {
     margin-top: 10px;
 }
 .el-aside{
-  
+  border: 10px;
+  background-color: 
 }
 .el-row {
   margin-bottom: 25px;
@@ -168,14 +164,18 @@ export default {
 .el-col {
   margin-bottom: 25px;
 }
-.el-main { 
- 
-}
+
 .common-layout{
-  background-color: azure;
+  background-color: #E6E6FA;
 }
 .el-header {
-
+    background-color: #F0FFFF;
 }
+
+.el-checkbox-button{
+    margin-left: 10px;
+    border: 110px;
+}
+
 
 </style>
